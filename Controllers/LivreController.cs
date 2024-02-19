48729a98708f29b0d3537915d2ee4e96caf0ccf3
hammami40 <hammamirami37@gmail.com>
2024-02-19 13:40:20 +0100

    first commit

---
+++ Controllers/LivreController.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using tp3.Models;
+
+namespace tp3.Controllers
+{
+    public class LivreController : Controller
+    {
+        public LivreController()
+        {
+            Livre l1 = new Livre(1, "L’étranger", "Albert Camus", "Cet\rouvrage\rmet en\rscène un\rpersonnage\rprincipal\rqui est\r\naussi le\rnarrateur,\rMeursault.", "1789654", new DateTime(1942 / 01 / 01), 25);
+            Livre.Livres.Add(l1);
+            Livre l2 = new Livre(2, "Voyage au\r\nbout de la\r\nnuit", "Louis-\r\nFerdinand\r\nCéline", "Cet\r\nouvrage est\r\nsouvent\r\nconsidéré\r\ncomme un\r\nexemple du\r\ncourant\r\nnihiliste.", "6593596", new DateTime(1932 / 01 / 01), 30);
+            Livre.Livres.Add(l2);
+        }
+        // GET: LivreController
+        public ActionResult Index()
+        {
+            return View(Livre.Livres);
+        }
+
+        // GET: LivreController/Details/5
+        public ActionResult Details(int id)
+        {
+            Livre livre1 = null;
+            foreach(Livre livre in Livre.Livres)
+                if (livre.Id == id)
+                {
+                    livre1 = livre; break;  
+                }
+            return View(livre1);
+        }
+
+        // GET: LivreController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: LivreController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: LivreController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: LivreController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: LivreController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: LivreController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Models/Livre.cs
@@ -0,0 +1,40 @@
+﻿namespace tp3.Models
+{
+    public class Livre
+    {
+        private int id;
+        private string isbn;
+        private DateTime datepublication;
+        private decimal prix;
+        private string titre;
+        private string auteur;
+        private string description;
+        private static List<Livre> livres = new List<Livre>();
+
+        public int Id { get => id; set => id = value; }
+        public string Titre { get => titre; set => titre = value; }
+        public string Auteur { get => auteur; set => auteur = value; }
+        public string Description { get => description; set => description = value; }
+        public string Isbn { get => isbn; set => isbn = value; }
+        public DateTime Datepublication { get => datepublication; set => datepublication = value; }
+        public decimal Prix { get => prix; set => prix = value; }
+        public static List<Livre> Livres { get => livres; set => livres = value; }
+
+        public Livre(int id, string titre, string auteur, string description, string isbn, DateTime datepublication, decimal prix)
+        {
+            this.Id = id;
+            this.Titre = titre;
+            this.Auteur = auteur;
+            this.Description = description;
+            this.Isbn = isbn;
+            this.Datepublication = datepublication;
+            this.Prix = prix;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is Livre livre &&
+                   id == livre.id;
+        }
+    }
+}
